127fddc0370be2f5469b57d2280715af1c130243
Frank Mayer <frank.mayer19072001@outlook.de>
2021-04-26 11:43:40 +0200

    Update loading.ts

--- src/script/loading.ts
+++ src/script/loading.ts
@@ -1,4 +1,4 @@
 setTimeout(() => {
   document.getElementById("loader")?.remove();
   document.body.parentElement?.classList.remove("loading");
-}, 750 + Math.random() * 500);
+}, Math.random() * 100);
