03aa0ee9f35d9c8e4d451139999a219bfba32ae0
Frank Mayer <frank.mayer1907@protonmail.com>
2022-04-29 23:18:59 +0200

    mail info

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ import type { messages } from "mailgun-js";
 
 const domain = "sandbox057dcab9050145cc8d2daa076a6adc70.mailgun.org";
 
+const recipients = ["mail@frank-mayer.io", "Easysix@easysix.de"];
+
 let mg: mailgun.Mailgun;
 
 export const message = functions.https.onRequest((request, response) => {
@@ -12,11 +14,14 @@ export const message = functions.https.onRequest((request, response) => {
     domain,
   });
 
+  const json = JSON.stringify(request.body, null, 2);
+
   const data: messages.SendData = {
     from: "Reost <no-reply@reost.de>",
-    to: "mail@frank-mayer.io",
+    to: recipients,
     subject: "Message from Reost",
-    text: JSON.stringify(request.body),
+    text: json,
+    html: `<pre>${json}</pre>`,
     "h:Reply-To": "no-reply@reost.de",
   };
 
