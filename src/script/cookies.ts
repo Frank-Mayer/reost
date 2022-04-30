350f439d375e62a83cc11a67b3182f3086bca470
Frank Mayer <frank.mayer1907@protonmail.com>
2022-04-30 17:02:40 +0200

    fixed cookieStore declaration

--- src/script/cookies.ts
+++ src/script/cookies.ts
@@ -1,7 +1,54 @@
-import { cookieStore } from "./lib/CookieStore";
-
 import { addDisposableEventListener, disposeNode } from "@frank-mayer/magic";
 import { analytics } from "./analytics";
+
+// #region CookieStore
+
+declare type cookieOptions = {
+  domain: string | null;
+  expires: number | null;
+  name: string;
+  path: string;
+  sameSite: string;
+  secure: boolean;
+  value: string;
+};
+
+declare interface CookieStore {
+  onchange: (event: Event) => void;
+
+  /**
+   * Gets a single cookie with the matching name.
+   */
+  get: (name: string) => Promise<cookieOptions>;
+
+  /**
+   * Gets all cookies with a matching name.
+   */
+  getAll: {
+    (name: string): Promise<{ [key: string]: cookieOptions }>;
+    (options: cookieOptions): Promise<{ [key: string]: cookieOptions }>;
+  };
+
+  /**
+   * Sets a cookie with the given name and value or options.
+   */
+  set: {
+    (name: string, value: string): Promise<void>;
+    (options: cookieOptions): Promise<void>;
+  };
+
+  /**
+   * Deletes a cookie with a matching name or options.
+   */
+  delete: {
+    (name: string): Promise<void>;
+    (options: cookieOptions): Promise<void>;
+  };
+}
+
+declare const cookieStore: CookieStore|undefined;
+
+// #endregion CookieStore
 
 (async()=>{
   if (cookieStore) {
