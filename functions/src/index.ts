6cf0dce583a7fd2309f1868c36065520d5c37adc
Frank Mayer <frank.mayer1907@protonmail.com>
2022-05-04 17:51:34 +0200

    removed confirmation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,62 +4,42 @@ import type { messages } from "mailgun-js";
 
 const domain = "sandbox057dcab9050145cc8d2daa076a6adc70.mailgun.org";
 
-const recipients = ["mail@frank-mayer.io", "Easysix@easysix.de"];
+const recipients = ["mail@frank-mayer.io"];
 
-let mg: mailgun.Mailgun;
+let mg: mailgun.Mailgun | undefined = undefined;
 
 const maxIdRand = 36 ** 2 - 1;
 
 export const message = functions.https.onRequest((request, response) => {
   const ticketId =
-  "#" +
-  Math.round(Math.random() * maxIdRand).toString(36) +
-  Date.now().toString(36);
+    "#" +
+    Math.round(Math.random() * maxIdRand).toString(36) +
+    Date.now().toString(36);
 
-  mg ??= mailgun({
-    apiKey: process.env.MAILGUN_API_KEY ?? "",
-    domain,
-  });
+  if (!mg) {
+    mg = mailgun({
+      apiKey: process.env.MAILGUN_API_KEY ?? "",
+      domain,
+    });
+  }
 
   const json = JSON.stringify(request.body, null, 2);
+
+  const dateTime = new Date().toLocaleString("de-DE");
 
   const data: messages.SendData = {
     from: "Reost <no-reply@reost.de>",
     to: recipients,
-    subject: "Message from Reost " + new Date().toLocaleString("de-DE"),
+    subject: "Reost " + ticketId,
     text: ticketId + "\n\n" + json,
-    html: `<p>Ticket${ticketId}</p><br/><pre>${json}</pre>`,
+    html: `<p>${dateTime}</p><br/><pre>${json}</pre>`,
     "h:Reply-To": "no-reply@reost.de",
   };
 
-  mg.messages().send(data, function(error, body) {
+  mg.messages().send(data, (error, body) => {
     console.log(error);
     console.log(body);
   });
-
-  if (typeof request.body.email === "string") {
-    const data2: messages.SendData = {
-      from: "Reost <no-reply@reost.de>",
-      to: request.body.email,
-      subject: "Deine Nachricht an Reost " + ticketId,
-      text: [
-        "Reost.de",
-        "Danke für Deine Nachricht!",
-        "Wir werden sie so schnell wie möglich bearbeiten und uns dann bei Dir melden!",
-      ].join("\n"),
-      html: [
-        "<h1>Reost.de</h1>",
-        "<p>Danke für Deine Nachricht!</p>",
-        "<p>Wir werden sie so schnell wie möglich bearbeiten und uns dann bei Dir melden!</p>",
-      ].join(""),
-      "h:Reply-To": "no-reply@reost.de",
-    };
-
-    mg.messages().send(data2, function(error, body) {
-      console.log(error);
-      console.log(body);
-    });
-  }
 
   response.redirect("https://reost.de/message-sent");
 });
