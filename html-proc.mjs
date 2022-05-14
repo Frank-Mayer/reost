94d61b51c189c7eb3741b52b79e7fb5d3a49f9dc
Frank Mayer <frank.mayer1907@protonmail.com>
2022-05-14 11:34:49 +0200

    auto ignore preprocessed files

--- html-proc.mjs
+++ html-proc.mjs
@@ -1,5 +1,5 @@
 import { existsSync, promises as fs } from "fs";
-import { join } from "path";
+import { join, resolve, relative } from "path";
 import { yahp } from "@frank-mayer/yahp";
 import showdown from "showdown";
 
@@ -15,6 +15,20 @@ const md = new showdown.Converter({
   underline: true,
 });
 
+const processDir = "./src";
+
+/**
+ * @type {Array<string>}
+ */
+const gitignore = [];
+
+/**
+ * @param {string} path
+ */
+const ignore = (path) => {
+  gitignore.push(relative(processDir, path));
+};
+
 // eslint-disable-next-line space-before-function-paren
 const fromDir = async (startPath) => {
   if (!existsSync(startPath)) {
@@ -29,18 +43,23 @@ const fromDir = async (startPath) => {
       await fromDir(filename);
     } else if (filename.endsWith(".yahp")) {
       console.debug(`Found input file ${filename}`);
+      const newFilename = filename.substring(0, filename.length - 4) + "html";
       await fs.writeFile(
-        filename.substring(0, filename.length - 4) + "html",
+        newFilename,
         await yahp(await fs.readFile(filename, "utf-8"))
       );
+      ignore(newFilename);
     } else if (filename.endsWith(".md")) {
       console.debug(`Found input file ${filename}`);
+      const newFilename = filename.substring(0, filename.length - 2) + "html";
       await fs.writeFile(
-        filename.substring(0, filename.length - 2) + "html",
+        newFilename,
         md.makeHtml(await fs.readFile(filename, "utf-8"))
       );
+      ignore(newFilename);
     }
   }
 };
 
-await fromDir("./src");
+await fromDir(processDir);
+await fs.writeFile(join(processDir, ".gitignore"), gitignore.join("\n"));
