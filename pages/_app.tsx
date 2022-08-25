250fe800d16213c06605324b05829ffbb509b9cd
Frank-Mayer <mail@frank-mayer.io>
2022-08-26 01:44:23 +0200

    basic ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { Shell } from "../components/Shell";
+import "../style/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+        respectReducedMotion: true,
+        defaultRadius: "md",
+        dir: "ltr",
+      }}
+    >
+      <Shell
+        nav={[
+          { href: "/", label: "Home" },
+          { href: "/discord", label: "Discord" },
+          { href: "/rules", label: "Regeln" },
+          { href: "/team", label: "Team" },
+          { href: "/contact", label: "Kontakt" },
+        ]}
+      >
+        <Component {...pageProps} />
+      </Shell>
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
