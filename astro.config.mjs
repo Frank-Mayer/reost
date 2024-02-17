353f4a2bf2ea3e6b3cca9789f3e8af1059b34e81
Frank Mayer <gh@frankmayer.dev>
2024-02-17 14:10:09 +0100

    out dir from env

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+const outDir = process.env.REOST_DE_DIR || "dist";
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [sitemap()],
     vite: {
         build: {
             assetsInlineLimit: 0,
-        }
-    }
+        },
+    },
+    outDir,
 });
